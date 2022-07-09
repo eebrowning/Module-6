54386a200356a1c2e84492f3e999b5fd51c7f94a
eebrowning <eebrowning828@gmail.com>
2022-07-08 18:07:14 -0600

    friday end of day

--- week-17/fib_eod.py
+++ week-17/fib_eod.py
@@ -18,7 +18,20 @@ def fib(num, dic={1: 0, 2: 1}):
         return dic[num]
 
 
-print(fib(45))
+# print(fib(45))
 # super fast!
 # space o(n)
 #  time o(n)
+
+
+# set1 = {1, 2, 3}
+# set2 = {2, 4, 6}
+# print(a - b)  # => {1, 3}
+# print(b - a)  # => {4, 6}
+# print(a ^ b)  # => {1, 3, 4, 6}
+
+set1 = {1, "hello", 3, 4}
+set2 = {"hello", 3, 6}
+print(set1 - set2)
+print(set1 ^ set2)
+print(set2 & set1)
---
+++ week-17/python-import-practice-main/python/office/idle.py
@@ -0,0 +1,11 @@
+def getWater():
+    print("Hydrating...")
+    print("Done")
+
+
+def useSocialMedia():
+    print("Tweeting...")
+    print("Done")
+
+
+# Your code here
