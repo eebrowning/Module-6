f3c37b88ee21848e815b257156cfedcbaf9a5a5d
eebrowning <eebrowning828@gmail.com>
2022-07-19 14:06:28 -0600

    small change

--- week-18/order-up/app/models.py
+++ week-18/order-up/app/models.py
@@ -1,17 +1,19 @@
-from flask_login import UserMixin          # New import
+from flask_login import UserMixin  # New import
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 db = SQLAlchemy()
 
-class Employee(db.Model, UserMixin):  # Your class definition
-    __tablename__= 'employees'# Mapping attributes, here
 
-    id= db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(100), nullable=False)
-    employee_number= db.Column(db.Integer, nullable=False, unique=True)
-    hashed_password= db.Column(db.String(255), nullable=False)
+class Employee(db.Model, UserMixin):  # Your class definition
+    __tablename__ = "employees"  # Mapping attributes, here
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    employee_number = db.Column(db.Integer, nullable=False, unique=True)
+    hashed_password = db.Column(db.String(255), nullable=False)
 
     @property
     def password(self):
@@ -26,44 +28,45 @@ class Employee(db.Model, UserMixin):  # Your class definition
 
 
 class Menu(db.Model, UserMixin):
-    __tablename__= 'menus'
+    __tablename__ = "menus"
 
-    id= db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(30), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
 
-    items= db.relationship("MenuItem", back_populates="menu",cascade="all, delete")
+    items = db.relationship("MenuItem", back_populates="menu", cascade="all, delete")
 
 
 class MenuItem(db.Model, UserMixin):
-    __tablename__= 'menu_items'
+    __tablename__ = "menu_items"
 
-    id= db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(100), nullable=False)
-    price= db.Column(db.Float, nullable=False)
-    menu_id=db.Column(db.Integer,  db.ForeignKey('menus.id'),nullable=False,)
-    menu_type_id=db.Column(db.Integer,db.ForeignKey('menu_item_types.id'), nullable=False)#, ForeignKey('menus.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    menu_id = db.Column(
+        db.Integer,
+        db.ForeignKey("menus.id"),
+        nullable=False,
+    )
+    menu_type_id = db.Column(
+        db.Integer, db.ForeignKey("menu_item_types.id"), nullable=False
+    )  # , ForeignKey('menus.id'))
 
-    menu= db.relationship("Menu", back_populates="items")
-    type= db.relationship("MenuItemType", back_populates='items')
-
+    menu = db.relationship("Menu", back_populates="items")
+    type = db.relationship("MenuItemType", back_populates="items")
 
 
 class MenuItemType(db.Model, UserMixin):
-    __tablename__= 'menu_item_types'
+    __tablename__ = "menu_item_types"
 
-    id= db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(20), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
 
-    items= db.relationship("MenuItem", back_populates="type",cascade="all, delete")
-
+    items = db.relationship("MenuItem", back_populates="type", cascade="all, delete")
 
 
 class Table(db.Model, UserMixin):
-    __tablename__= 'tables'
+    __tablename__ = "tables"
 
-    id= db.Column(db.Integer, primary_key=True)
-    number=db.Column(db.String(20), nullable=False, unique=True)
-    capacity=db.Column(db.String(20), nullable=False)
-
-
-   
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.String(20), nullable=False, unique=True)
+    capacity = db.Column(db.String(20), nullable=False)
